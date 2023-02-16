4cb2e7f310d3582acf8caa1795d7615d6b5abca7
M AKIF <makifhussainmn@gmail.com>
2023-02-16 22:06:22 +0500

    aaf

--- app.js
+++ app.js
@@ -6,3 +6,27 @@ case 1:
   document.write(num1);
  break;
 }
+
+document.getElementById("buttonak").onclick = function(){
+  alert("akif")
+  
+  
+  }
+  document.getElementById("buttonak").onclick = function(){
+      console.log("akif")
+      
+      
+      }
+  
+      document.getElementById("buttonak").onclick = function(){
+   
+          
+          
+          }
+  
+  
+  document.getElementById("ahmed").onclick=function(){
+  
+  
+  }     
+  
