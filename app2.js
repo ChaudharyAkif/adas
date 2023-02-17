283a6f253164f96c654b98efb69d39fec897e21e
M AKIF <makifhussainmn@gmail.com>
2023-02-17 21:27:15 +0500

    last commit ak

---
+++ app2.js
@@ -0,0 +1,18 @@
+document.getElementById("akif").onclick = function(){
+
+    let message = "Good day ";
+    let name = "akif";
+    let banger ="!";
+    
+    console.log ( message + name + banger);
+    document.getElementById("aa").innerHTML = " akif"
+    
+    }
+    
+    
+    
+    let a = prompt+1;
+    let b = 10;
+    
+    
+    
