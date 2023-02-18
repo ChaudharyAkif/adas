51f04d2128254eff8db706b2f9a159c1bc7b2374
M AKIF <makifhussainmn@gmail.com>
2023-02-18 22:28:05 +0500

    12525commit

--- app.js
+++ app.js
@@ -1,76 +1,76 @@
-// Switch (num1>num2) {
-// case 0:
-//   document.write(num2);
-//   break;
-// case 1:
-//   document.write(num1);
-//  break;
-// }
+Switch (num1>num2) {
+case 0:
+  document.write(num2);
+  break;
+case 1:
+  document.write(num1);
+ break;
+}
 
-// document.getElementById("buttonak").onclick = function(){
-//   alert("akif")
+document.getElementById("buttonak").onclick = function(){
+  alert("akif")
   
   
-//   }
-//   document.getElementById("buttonak").onclick = function(){
-//       console.log("akif")
+  }
+  document.getElementById("buttonak").onclick = function(){
+      console.log("akif")
       
       
-//       }
+      }
   
-//       document.getElementById("buttonak").onclick = function(){
+      document.getElementById("buttonak").onclick = function(){
    
           
           
-//           }
+          }
   
   
-//   document.getElementById("ahmed").onclick=function(){
+  document.getElementById("ahmed").onclick=function(){
   
   
-//   }  
-//  alert("akif")   
+  }  
+ alert("akif")   
 
-// let a = prompt ("enter your age")
-//   const item ={
-//  "chaudhary" : true,
-//  "akif": false,
-//  "abdullH": undefined
-
-
-
-// }  console(item ["chaudhary"])
+let a = prompt ("enter your age")
+  const item ={
+ "chaudhary" : true,
+ "akif": false,
+ "abdullH": undefined
 
 
 
-// let a = prompt (" Enter your age" )
-// if (a >=80 && a <=100){
-// alert ("A+")}
+}  console(item ["chaudhary"])
+
+
+
+let a = prompt (" Enter your age" )
+if (a >=80 && a <=100){
+alert ("A+")}
 
 
   
-//  else if (a >=70 && a <=90){
-//     alert ("A")}
+ else if (a >=70 && a <=90){
+    alert ("A")}
     
-//     else  if (a >=70 && a <=80){
-//       alert ("b")}
+    else  if (a >=70 && a <=80){
+      alert ("b")}
       
-//       else   if (a >=60 && a <=70){
-//         alert ("c")}
+      else   if (a >=60 && a <=70){
+        alert ("c")}
         
-//         else  if (a >=50 && a <=60){
-//           alert ("d")}
-//           else  if (a >=40 && a <=50){
-//             alert ("e")}
-//             else  if (a >=0 && a <=30){
-//               alert ("f")}
+        else  if (a >=50 && a <=60){
+          alert ("d")}
+          else  if (a >=40 && a <=50){
+            alert ("e")}
+            else  if (a >=0 && a <=30){
+              alert ("f")}
                  
 
 
-// else {
+else {
 
-//   alert("you are not correct" )
-// }
+  alert("you are not correct" )
+}
 
 
 
@@ -78,11 +78,11 @@
 
 
 var students = ["akif","ahmed","chaudhary"]
-// // students.pop()
-// students.push("abdullaha")
-// students.shift()
-// //  students.unshift("arslan")
-// students.splice(0,2,"bbb")
+// students.pop()
+students.push("abdullaha")
+students.shift()
+//  students.unshift("arslan")
+students.splice(0,2,"bbb")
 console.log(students)
 
 alert ( "WEllcome" +""+students )
